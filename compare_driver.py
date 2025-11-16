309928f79966074b100bed1b714f65de85b3089d
Tim Peters <tim.peters@gmail.com>
2025-11-16 12:57:33 -0600

    Generate candidate names from about 155K plausible Unicode characters.

--- compare_driver.py
+++ compare_driver.py
@@ -1,7 +1,28 @@
-import json, random, subprocess, sys
+import json, random, subprocess, sys, unicodedata
 from permute import permute  # Python implementation
 
+# sys.stdout.reconfigure(encoding='utf-8')
+
+printable_chars = tuple(
+    filter(
+        lambda ch: ch.isprintable()
+                   and not unicodedata.category(ch).startswith('C'),
+        map(chr, range(0x110000))
+    )
+)
+print("universe has",
+      format(len(printable_chars), "_"),
+      "code points")
+
+def random_unicode_string(length=10):
+    return ''.join(random.choices(printable_chars, k=length))
+
 def random_score_dict(num_candidates=10, max_score=500):
+    # Random candidate names from full plausibie Unicode set.
+    return {random_unicode_string(random.randint(1, 30)):
+            random.randint(0, max_score)
+            for i in range(num_candidates)}
+
     # Generate candidate names like "C0", "C1", ...
     return {f"C{i}": random.randint(0, max_score)
             for i in range(num_candidates)}
@@ -25,7 +46,7 @@ def run_node(score):
         raise
     return json.loads(proc.stdout)
 
-def main():
+def main(output=None):
     trials = 1000
     for t in range(trials):
         score = random_score_dict(num_candidates=12, max_score=5000)
@@ -37,10 +58,15 @@ def main():
             print("Python:", py_perm)
             print("Node:  ", node_perm)
             sys.exit(1)
-        print(py_perm)
+        if output:
+            print(py_perm, file=output)
         print(t+1, "of", trials, "done", end="\r")
     print()
     print(f"All {trials} trials matched")
 
 if __name__ == "__main__":
-    main()
+    if 0: # enable this for some debugging output
+        with open("output.txt", "w", encoding="utf-8") as f:
+            main(f)
+    else:
+        main()
