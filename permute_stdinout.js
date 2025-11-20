245e4d9eeebe352699262d30f9f0bff721bcc40d
Tim Peters <tim.peters@gmail.com>
2025-11-20 12:13:50 -0600

    Add comments.

--- permute_stdinout.js
+++ permute_stdinout.js
@@ -1,3 +1,8 @@
+// Helper to run the Node version _from_ Python.
+// Read a JSON representation of permute's arguments
+// from stdin, and write the permutation to stdout,
+// again as JSON.
+// Python invokes this via the subprocess module.
 const fs = require('fs');
 const {permute} = require('./permute.js');
 
