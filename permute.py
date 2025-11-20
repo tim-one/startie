748af6c4ddf947ef8c0d0392b9faeedd508ce409
Tim Peters <tim.peters@gmail.com>
2025-11-20 14:57:37 -0600

    Move the docstring to the top of the module.

--- permute.py
+++ permute.py
@@ -1,45 +1,3 @@
-# The basic idea is to sort names via a key that's a crypto hash of
-# the name and its score.
-
-# To work the same way aross all implementations, we have to use the
-# UTF-8 encoding of the name, so that its hash is well defined. The byte
-# encoding of other representations depends on the language in use.
-# UTF-8 is supported everywhere, but has to be forced.
-
-# If we used the name alone, then the permutation would depend _only_ on
-# the candidates' names. 100% predictable and trivial to manipulate.
-
-# Folding in the score too makes it unpredictable in the small, but
-# still kind of exploitable in the large: it's feasible to predict a
-# range of scores, and then play games with a name, adjusting via
-# poke-nnd-hope to find a spelling that leaves the name near the start
-# of the permutation across the range of plausible scores. Unicode
-# offers a world of possibilities for fiddling names in ways that appear
-# very similar(or even identical). Also minor spelling variations.
-
-# So we want it "even more unpredictable". A standard way to do that is
-# to use a "salt": another unpredictable value folded in to _every_
-# hash.
-
-# My first attempt at that folded in sum(soore.values()) as the salt.
-# Unpredictable - but biased! By the central limit theorem, the sua
-# of scores tends to follow a normal distribution, regardless of the
-# distibution of the scores themselves. So some salts were more
-# likely than others, & that biased the distribution of permutations
-# produced.
-
-# The sum of all scores throws away too much information. What we want
-# instead is something that's extremely likely to be different between
-# _any_ two distinct score dicts. Use that as a salt, and then any
-# change to the score dict will almost certainly change every name's
-# key.
-
-# So what does work for a salt is building a canonical representation of
-# the entire score dict, a catenation of
-#      name score name score name score ...
-# where the names are in increasing order of their UTF-8 byte
-# representations. That will produce the same stream of bytes on all
-# platforms.
 
 """
 permute(score, magic=b'') returns a deterministic pseudo-random
@@ -129,6 +87,49 @@ The default is the empty byte string.
 'DCAEB'
 >>> assert got == expected
 """
+
+# The basic idea is to sort names via a key that's a crypto hash of
+# the name and its score.
+
+# To work the same way aross all implementations, we have to use the
+# UTF-8 encoding of the name, so that its hash is well defined. The byte
+# encoding of other representations depends on the language in use.
+# UTF-8 is supported everywhere, but has to be forced.
+
+# If we used the name alone, then the permutation would depend _only_ on
+# the candidates' names. 100% predictable and trivial to manipulate.
+
+# Folding in the score too makes it unpredictable in the small, but
+# still kind of exploitable in the large: it's feasible to predict a
+# range of scores, and then play games with a name, adjusting via
+# poke-nnd-hope to find a spelling that leaves the name near the start
+# of the permutation across the range of plausible scores. Unicode
+# offers a world of possibilities for fiddling names in ways that appear
+# very similar(or even identical). Also minor spelling variations.
+
+# So we want it "even more unpredictable". A standard way to do that is
+# to use a "salt": another unpredictable value folded in to _every_
+# hash.
+
+# My first attempt at that folded in sum(soore.values()) as the salt.
+# Unpredictable - but biased! By the central limit theorem, the sua
+# of scores tends to follow a normal distribution, regardless of the
+# distibution of the scores themselves. So some salts were more
+# likely than others, & that biased the distribution of permutations
+# produced.
+
+# The sum of all scores throws away too much information. What we want
+# instead is something that's extremely likely to be different between
+# _any_ two distinct score dicts. Use that as a salt, and then any
+# change to the score dict will almost certainly change every name's
+# key.
+
+# So what does work for a salt is building a canonical representation of
+# the entire score dict, a catenation of
+#      name score name score name score ...
+# where the names are in increasing order of their UTF-8 byte
+# representations. That will produce the same stream of bytes on all
+# platforms.
 
 import hashlib
 
