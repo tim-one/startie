f4fc1b443e67e465c1edbaae5d2863fb4b72d8d5
Tim Peters <tim.peters@gmail.com>
2025-11-20 13:29:06 -0600

    Capture some actual output to make sure it doesn't change.

--- compare_driver.py
+++ compare_driver.py
@@ -1,7 +1,59 @@
-from run_node import node_permute
-import random, sys, secrets
+import random, sys, secrets, doctest
 from itertools import filterfalse
 from permute import permute  # Python implementation
+from run_node import node_permute
+
+__test__ = {'main':
+"""
+Capture some actual outputs to make sure results remain the same.
+
+>>> random.seed(32476783687364)
+>>> import string
+>>> candidates = list(string.ascii_uppercase)
+>>> for i in range(10):
+...     cands = random.sample(candidates, random.randrange(2, 11))
+...     scores = [random.randrange(100)
+...               for i in range(len(cands))]
+...     score = dict(zip(cands, scores))
+...     magicint = random.randrange(1 << 64)
+...     magic = magicint.to_bytes(8, 'little')
+...     print("score", score)
+...     print("magic 0x" + magic.hex())
+...     py_result = permute(score, magic)
+...     js_result = node_permute(score, magic)
+...     print(py_result, py_result == js_result)
+...     assert py_result == js_result
+score {'D': 44, 'H': 43, 'V': 73, 'K': 47, 'G': 47, 'R': 7}
+magic 0x6534defa3e6bdf69
+['G', 'K', 'H', 'V', 'D', 'R'] True
+score {'A': 28, 'H': 15, 'F': 27, 'V': 54}
+magic 0x00c30bfae3baab68
+['V', 'H', 'F', 'A'] True
+score {'X': 76, 'D': 32, 'H': 4, 'E': 85, 'G': 59, 'L': 96, 'J': 22, 'N': 38, 'C': 43, 'T': 69}
+magic 0xb1fb8ce49ee5e1db
+['X', 'T', 'C', 'J', 'H', 'D', 'L', 'G', 'E', 'N'] True
+score {'S': 14, 'T': 81, 'C': 91, 'O': 74, 'Q': 2}
+magic 0xd13223588fda6ca4
+['Q', 'S', 'T', 'C', 'O'] True
+score {'M': 26, 'Y': 44, 'W': 18}
+magic 0x27adc9eb95fa0627
+['W', 'Y', 'M'] True
+score {'Z': 57, 'L': 61, 'D': 80, 'C': 30, 'M': 95, 'T': 78, 'V': 38, 'G': 57, 'Q': 47, 'K': 38}
+magic 0x3d7beac4badba914
+['G', 'D', 'L', 'V', 'K', 'M', 'Q', 'Z', 'C', 'T'] True
+score {'D': 33, 'Q': 91, 'K': 28, 'Y': 70, 'O': 21, 'B': 60}
+magic 0xd0eae35067feb7be
+['B', 'D', 'Y', 'Q', 'K', 'O'] True
+score {'F': 36, 'G': 52}
+magic 0x4ecc14afce878d1c
+['G', 'F'] True
+score {'Y': 76, 'P': 45, 'O': 8, 'M': 63, 'W': 97, 'V': 87}
+magic 0xe264c8c0a070662a
+['P', 'M', 'O', 'V', 'W', 'Y'] True
+score {'L': 32, 'M': 61, 'N': 84, 'U': 93, 'S': 34}
+magic 0x4afb51805314e9f9
+['L', 'M', 'S', 'U', 'N'] True"""
+}
 
 FORBIDDEN_CODEPOINTS = range(0xD800, 0xDFFF + 1) # surrogates
 print(format(len(FORBIDDEN_CODEPOINTS), '_'),
@@ -19,6 +71,11 @@ if 1:
             assert ISBADCP(i), (i, hex(i))
         else:
             assert not ISBADCP(i), (i, hex(i))
+
+if __name__ == "__main__":
+    import doctest
+    outcome = doctest.testmod()
+    print("doctest result:", outcome)
 
 def random_unicode_string(length=10):
     assert length >= 0
