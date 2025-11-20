6b4edb65d779295c09b9f2cbc1743174b032fc43
Tim Peters <tim.peters@gmail.com>
2025-11-20 00:19:48 -0600

    Ha! Switched to a tiny bit of more idiomaic JavaScript!

--- permute_test.js
+++ permute_test.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const permute = require('./permute.js');
+const {permute} = require('./permute.js');
 
 // Read JSON dict from stdin
 const input = fs.readFileSync(0, 'utf8');
 const args = JSON.parse(input);
 
 // Compute permutation
-const result = permute.permute(args.score, Buffer.from(args.magic));
+const result = permute(args.score, Buffer.from(args.magic));
 
 // Write result back to stdout as JSON
 process.stdout.write(JSON.stringify(result));
